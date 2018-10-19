e6b6e84d732ac001e86ef088b9f3648a3ccc069a
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2018-10-19 11:03:53 -0700

    Add files via upload
    with menu

--- arielleB.cpp
+++ arielleB.cpp
@@ -1,23 +1,24 @@
 //Arielle Battle
 //Arielle's game source code
-//Description: my source code contains the function for showing my name and 
-//	       my picture in the main walk2.cpp file
+//Description: my source code contains the function for showing my name, 
+//	       my picture, and the jump function in the main walk2.cpp file
 
 #include "fonts.h"
 #include <GL/glx.h>
+#include <cmath>
 
 void ShowArielleName(int x, int y)
 {
-    Rect r;
-    r.bot = y - 100;
-    r.left = x + 280;
-    r.center = 1;
-    ggprint8b(&r, 40, 0x00ffff44, "Arielle Battle");
+	Rect r;
+	r.bot = y - 100;
+	r.left = x + 280;
+	r.center = 1;
+	ggprint8b(&r, 40, 0x00ffff44, "Arielle Battle");
 }
 
 void showAriellePic(int x, int y, GLuint texid)
 {
-    static float angle2 = 0.0f;
+	static float angle2 = 0.0f;
 
 	glColor3ub(225, 255, 255);
 	int wid = 40;
@@ -27,12 +28,62 @@ void showAriellePic(int x, int y, GLuint texid)
 	angle2 += 1;	//the speed of the picture
 	glBindTexture(GL_TEXTURE_2D, texid);
 	glBegin(GL_QUADS);
-		glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
-		glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-		glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
-		glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
 	glEnd();
 	glPopMatrix();
 }
 
+void menu (int x, int y)
+{
+	Rect a;
+	Rect b;
+	Rect c;
+	Rect d;
+	
 
+	a.bot = y;
+	a.left = x + 280;
+	a.center = 1;
+
+	b.bot = y - 100;
+	b.left = x + 280;
+	b.center = 1;
+
+	c.bot = y - 150;
+	c.left = x + 280;
+	c.center = 1;
+
+	d.bot = y - 200;
+	d.left = x + 280;
+	d.center = 1;
+
+	ggprint8b(&a, 40, 0x00ffff44, "PERDITION");
+	ggprint8b(&b, 40, 0x00ffff44, "PRESS S TO START");
+	ggprint8b(&c, 40, 0x00ffff44, "PRESS E TO EXIT");
+	ggprint8b(&d, 40, 0x00ffff44, "PRESS C FOR CREDITS");
+}
+
+/*void jump (const float gravity, const float timeslice)
+  {
+  float positionX, positionY; //position of character
+  float velocityX, 
+  float velocityY = -12.0f; //should make jump same height each jump
+//float gravity = 0.5f;
+do
+{
+positionX += velocityX * time; //make h.v. to x position
+position += velocirtY * time; //make v.v. to x pos
+velocityY += gravity * time; //apply gravity to come back down
+}while (getchar() != 32 || getchar() != ' ');
+}*/
+
+/*void init_opengl(int x, int y)
+  {
+  }*/
+/*void ccollision ()
+  {
+
+  }*/
