e71f2c9ea36cfbc257a56acc20526f197bb4ac47
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2018-10-18 23:36:58 -0700

    Add files via upload
    update for menu

--- arielleB.cpp
+++ arielleB.cpp
@@ -8,17 +8,16 @@
 
 void ShowArielleName(int x, int y)
 {
-    Rect r;
-    r.bot = y - 100;
-    r.left = x + 280;
-    r.center = 1;
-    ggprint(&r, 40, 0x00ffff44, "Arielle Battle");
-
+	Rect r;
+	r.bot = y - 100;
+	r.left = x + 280;
+	r.center = 1;
+	ggprint8b(&r, 40, 0x00ffff44, "Arielle Battle");
 }
 
 void showAriellePic(int x, int y, GLuint texid)
 {
-    static float angle2 = 0.0f;
+	static float angle2 = 0.0f;
 
 	glColor3ub(225, 255, 255);
 	int wid = 40;
@@ -28,44 +27,62 @@ void showAriellePic(int x, int y, GLuint texid)
 	angle2 += 1;	//the speed of the picture
 	glBindTexture(GL_TEXTURE_2D, texid);
 	glBegin(GL_QUADS);
-		glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
-		glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-		glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
-		glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
 	glEnd();
 	glPopMatrix();
 }
 
 void menu (int x, int y)
 {
-	rect r;
-	r.bot = y - 100;
-	r.left = x +260;
-	r.center = 1;
-	ggprint(&r, 40, 0x00ffff44, "PERDITION"); 
-	ggprint(&r, 40, 0x00ffff44, "START");
-	ggprint(&r, 40, 0x00ffff44, "EXIT");
-	ggprint(&r, 40, 0x00ffff44, "CREDITS");	
+	Rect a;
+	Rect b;
+	Rect c;
+	Rect d;
+	
+
+	a.bot = y;
+	a.left = x;
+	a.center = 1;
+
+	b.bot = y - 100;
+	b.left = x + 280;
+	b.center = 1;
+
+	c.bot = y - 200;
+	c.left = x + 380;
+	c.center = 1;
+
+	d.bot = y - 300;
+	d.left = x + 480;
+	d.center = 1;
+
+	ggprint8b(&a, 40, 0x00ffff44, "PERDITION");
+	ggprint8b(&b, 40, 0x00ffff44, "START");
+	ggprint8b(&c, 40, 0x00ffff44, "EXIT");
+	ggprint8b(&d, 40, 0x00ffff44, "CREDITS");
 }
 
 /*void jump (const float gravity, const float timeslice)
+  {
+  float positionX, positionY; //position of character
+  float velocityX, 
+  float velocityY = -12.0f; //should make jump same height each jump
+//float gravity = 0.5f;
+do
 {
-    float positionX, positionY; //position of character
-    float velocityX, 
-    float velocityY = -12.0f; //should make jump same height each jump
-    //float gravity = 0.5f;
-	do
-	{
-	    positionX += velocityX * time; //make h.v. to x position
-	    position += velocirtY * time; //make v.v. to x pos
-	    velocityY += gravity * time; //apply gravity to come back down
-	}while (getchar() != 32 || getchar() != ' ');
+positionX += velocityX * time; //make h.v. to x position
+position += velocirtY * time; //make v.v. to x pos
+velocityY += gravity * time; //apply gravity to come back down
+}while (getchar() != 32 || getchar() != ' ');
 }*/
 
 /*void init_opengl(int x, int y)
-{
-}*/
+  {
+  }*/
 /*void ccollision ()
-{
-	
-}*/
+  {
+
+  }*/
