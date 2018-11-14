2333e0dc5207dd14ba6acd74c25ffaf71a3448c2
Theodore Barcelona <42781037+tbarcelona@users.noreply.github.com>
2018-11-14 10:56:22 -0800

    Add files via upload

--- theodoreB.cpp
+++ theodoreB.cpp
@@ -3,11 +3,11 @@
 //Group #: 2
 //Updated: 10/8/18
 //Theo's game source code
-//Description: show name/picture in credits
+//Description: show name/picture in credits, plays sounds/music
 #include <math.h>
 #include <GL/glx.h>
 #include "fonts.h"
-//Sound that will be implemented later
+//Sound library
 //#include <stdio.h>
 //#include <string.h>
 //#include <unistd.h>
@@ -45,7 +45,65 @@ void showTheodorePicture(int x, int y, GLuint texid)
     glEnd();
     glPopMatrix();
 }
+//Sound Implementation
+//Original Source: OpenAl example program
+//Author: Gordon Griesel
 //void soundTest(int x,  int y)
 //{
-// Sound will be implemented here.
+    // Soundtest only
+//#ifdef USE_OPENAL_SOUND
+//    alutInit(0, NULL);
+//    if (alGetError() != AL_NO_ERROR) {
+//	printf("ERROR: alutInit()\n");
+//	return 0;
+//    }
+//    //Clear error state.
+//    alGetError();
+    //
+    //Setup the listener.
+    //Forward and up vectors are used.
+//    float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
+//    alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
+//    alListenerfv(AL_ORIENTATION, vec);
+//    alListenerf(AL_GAIN, 1.0f);
+    //
+    //Buffer holds the sound information.
+//    ALuint alBuffer;
+//    alBuffer = alutCreateBufferFromFile("./test.wav");
+    //
+    //Source refers to the sound.
+//    ALuint alSource;
+    //Generate a source, and store it in a buffer.
+//    alGenSources(1, &alSource);
+//    alSourcei(alSource, AL_BUFFER, alBuffer);
+    //Set volume and pitch to normal, no looping of sound.
+//    alSourcef(alSource, AL_GAIN, 1.0f);
+//    alSourcef(alSource, AL_PITCH, 1.0f);
+//    alSourcei(alSource, AL_LOOPING, AL_FALSE);
+//    if (alGetError() != AL_NO_ERROR) {
+//	printf("ERROR: setting source\n");
+//	return 0;
+//    }
+//    for (int i=0; i<4; i++) {
+//	alSourcePlay(alSource);
+//	usleep(250000);
+//    }
+    //Cleanup.
+    //First delete the source.
+//    alDeleteSources(1, &alSource);
+    //Delete the buffer.
+//    alDeleteBuffers(1, &alBuffer);
+    //Close out OpenAL itself.
+    //Get active context.
+//    ALCcontext *Context = alcGetCurrentContext();
+    //Get device for active context.
+//    ALCdevice *Device = alcGetContextsDevice(Context);
+    //Disable context.
+//    alcMakeContextCurrent(NULL);
+    //Release context(s).
+//    alcDestroyContext(Context);
+    //Close device.
+//    alcCloseDevice(Device);
+//#endif
+    
 //}
