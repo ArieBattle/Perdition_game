82866df9ae9df4be6c0c822f19a89a9a5d5fc2be
Theodore Barcelona <42781037+tbarcelona@users.noreply.github.com>
2018-11-27 12:17:14 -0800

    Update theodoreB.cpp

--- theodoreB.cpp
+++ theodoreB.cpp
@@ -105,4 +105,45 @@ void sound_test()
     return;
 //#endif
 }
+
+//void walking_sound()
+    //Intiate walking loop sounds
+//{
+//    alutInit(0, NULL);
+//    if (alGetError() != AL_NO_ERROR) {
+//      printf("ERROR: alutInit()\n");
+//    }
+    //Clear error state.
+//    alGetError();
+
+    //Setup the listener.
+    //Forward and up vectors are used.
+//    float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
+//    alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
+//    alListenerfv(AL_ORIENTATION, vec);
+//    alListenerf(AL_GAIN, 1.0f);
+
+    //Buffer holds the sound information.
+//    ALuint alBuffer;
+//    alBuffer = alutCreateBufferFromFile("test.wav");
+
+    //Source refers to the sound.
+//    ALuint alSource;
+    //Generate a source, and store it in a buffer.
+//    alGenSources(1, &alSource);
+//    alSourcei(alSource, AL_BUFFER, alBuffer);
+    //Set volume and pitch to normal, no looping of sound.
+//    alSourcef(alSource, AL_GAIN, 1.0f);
+//    alSourcef(alSource, AL_PITCH, 1.0f);
+//    alSourcei(alSource, AL_LOOPING, AL_FALSE);
+//    if (alGetError() != AL_NO_ERROR) {
+//      printf("ERROR: setting source\n");
+//    }
+//    for (int i=0; i<42; i++) {
+//      alSourcePlay(alSource);
+//      usleep(500000);
+//    }
+
+//}
+
     
