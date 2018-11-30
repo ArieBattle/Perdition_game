6d45727dad3f0e99630b5d942cecf0c03d08bd3a
Theodore Barcelona <42781037+tbarcelona@users.noreply.github.com>
2018-11-30 12:04:42 -0800

    Add files via upload

--- theodoreB.cpp
+++ theodoreB.cpp
@@ -7,7 +7,6 @@
 #include <math.h>
 #include <GL/glx.h>
 #include "fonts.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -48,7 +47,9 @@ void showTheodorePicture(int x, int y, GLuint texid)
 //Sound Implementation
 //Original Source: OpenAl example program
 //Original Author: Gordon Griesel
-void sound_test()
+ALuint alBuffer[4];
+ALuint alSource[4];
+void init_sounds()
 {
     // Soundtest only
 //#ifdef USE_OPENAL_SOUND
@@ -67,81 +68,111 @@ void sound_test()
     alListenerf(AL_GAIN, 1.0f);
 
     //Buffer holds the sound information.
-    ALuint alBuffer;
-    alBuffer = alutCreateBufferFromFile("test.wav");
+    //ALuint alBuffer[4];
+    alBuffer[0] = alutCreateBufferFromFile("./audio/test.wav");
+    alBuffer[1] = alutCreateBufferFromFile("./audio/03_Deserted_Mansion.wav");
+    alBuffer[2] = alutCreateBufferFromFile("./audio/Move1.wav");
+    alBuffer[3] = alutCreateBufferFromFile("./audio/Monster6.wav");
 
     //Source refers to the sound.
-    ALuint alSource;
+    //ALuint alSource[4];
     //Generate a source, and store it in a buffer.
-    alGenSources(1, &alSource);
-    alSourcei(alSource, AL_BUFFER, alBuffer);
+    alGenSources(4, alSource);
     //Set volume and pitch to normal, no looping of sound.
-    alSourcef(alSource, AL_GAIN, 1.0f);
-    alSourcef(alSource, AL_PITCH, 1.0f);
-    alSourcei(alSource, AL_LOOPING, AL_FALSE);
+    
+    alSourcei(alSource[0], AL_BUFFER, alBuffer[0]);
+    alSourcef(alSource[0], AL_GAIN, 1.0f);
+    alSourcef(alSource[0], AL_PITCH, 1.0f);
+    alSourcef(alSource[0], AL_LOOPING, AL_FALSE);
     if (alGetError() != AL_NO_ERROR) {
         printf("ERROR: setting source\n");
     }
-    //for (int i=0; i<4; i++) {
-        alSourcePlay(alSource);
-    //    usleep(500000);
-    //}
-    //Cleanup.
-    //First delete the source.
-    //alDeleteSources(1, &alSource);
-    //Delete the buffer.
-    //alDeleteBuffers(1, &alBuffer);
-    //Close out OpenAL itself.
-    //Get active context.
-    //ALCcontext *Context = alcGetCurrentContext();
-    //Get device for active context.
-    //ALCdevice *Device = alcGetContextsDevice(Context);
-    //Disable context.
-    //alcMakeContextCurrent(NULL);
-    //Release context(s).
-    //alcDestroyContext(Context);
-    //Close device.
-    //alcCloseDevice(Device);
-    return;
+
+    alSourcei(alSource[1], AL_BUFFER, alBuffer[1]);
+    alSourcef(alSource[1], AL_GAIN, 1.0f);
+    alSourcef(alSource[1], AL_PITCH, 1.0f);
+    alSourcef(alSource[1], AL_LOOPING, AL_TRUE);
+    if (alGetError() != AL_NO_ERROR) {
+        printf("ERROR: setting source\n");
+    }
+
+    alSourcei(alSource[2], AL_BUFFER, alBuffer[2]);
+    alSourcef(alSource[2], AL_GAIN, 1.0f);
+    alSourcef(alSource[2], AL_PITCH, 1.0f);
+    alSourcef(alSource[2], AL_LOOPING, AL_FALSE);
+    if (alGetError() != AL_NO_ERROR) {
+        printf("ERROR: setting source\n");
+    }
+
+    alSourcei(alSource[3], AL_BUFFER, alBuffer[3]);
+    alSourcef(alSource[3], AL_GAIN, 1.0f);
+    alSourcef(alSource[3], AL_PITCH, 1.0f);
+    alSourcef(alSource[3], AL_LOOPING, AL_FALSE);
+    if (alGetError() != AL_NO_ERROR) {
+        printf("ERROR: setting source\n");
+    }
 //#endif
 }
 
-//void walking_sound()
+void sound_test()
+{
+    //Sound Test only. Plays when a button is pressed.
+    alSourcePlay(alSource[0]);
+    printf("Sound is playing.\n");
+}
+
+void music()
+{
+    //Plays a music theme.
+    alSourcePlay(alSource[1]);
+    printf("Music playing.\n");
+}
+
+
+
+void walking_sound()
+{
     //Initiates walking loop sounds
+    alSourcePlay(alSource[2]);
+    printf("walking sound.\n");
+}
+
+//void enemy_sound()
 //{
-//    alutInit(0, NULL);
-//    if (alGetError() != AL_NO_ERROR) {
-//      printf("ERROR: alutInit()\n");
-//    }
-    //Clear error state.
-//    alGetError();
-
-    //Setup the listener.
-    //Forward and up vectors are used.
-//    float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
-//    alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
-//    alListenerfv(AL_ORIENTATION, vec);
-//    alListenerf(AL_GAIN, 1.0f);
-
-    //Buffer holds the sound information.
-//    ALuint alBuffer;
-//    alBuffer = alutCreateBufferFromFile("test.wav");
-
-    //Source refers to the sound.
-//    ALuint alSource;
-    //Generate a source, and store it in a buffer.
-//    alGenSources(1, &alSource);
-//    alSourcei(alSource, AL_BUFFER, alBuffer);
-    //Set volume and pitch to normal, no looping of sound.
-//    alSourcef(alSource, AL_GAIN, 1.0f);
-//    alSourcef(alSource, AL_PITCH, 1.0f);
-//    alSourcei(alSource, AL_LOOPING, AL_FALSE);
-//    if (alGetError() != AL_NO_ERROR) {
-//      printf("ERROR: setting source\n");
-//    }
-//    for (int i=0; i<42; i++) {
-//      alSourcePlay(alSource);
-//      usleep(500000);
-//    }
-
+//The enemy makes a sound if the player collides with an enemy.
 //}
+
+//void jump_sound()
+//{
+//Plays when a button is pressed.
+//
+//}
+
+extern void close_sounds()
+{
+
+    //Cleanup.
+    //First delete the source.
+    alDeleteSources(1, &alSource[0]);
+    alDeleteSources(1, &alSource[1]);
+    alDeleteSources(1, &alSource[2]);
+    alDeleteSources(1, &alSource[3]);
+    //Delete the buffer.
+    alDeleteBuffers(1, &alBuffer[0]);
+    alDeleteBuffers(1, &alBuffer[1]);
+    alDeleteBuffers(1, &alBuffer[2]);
+    alDeleteBuffers(1, &alBuffer[3]);
+
+    //Close out OpenAL itself.
+    //Get active context.
+    ALCcontext *Context = alcGetCurrentContext();
+    //Get device for active context.
+    ALCdevice *Device = alcGetContextsDevice(Context);
+    //Disable context.
+    alcMakeContextCurrent(NULL);
+    //Release context(s).
+    alcDestroyContext(Context);
+    //Close device.
+    alcCloseDevice(Device);
+
+}
